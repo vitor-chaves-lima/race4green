09589d003437bb8cf2b6500df9a6ca611709dc22
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-01 20:47:24 -0300

    Corrigindo nome do endpoint

--- backend/integrations_service/app/router.py
+++ backend/integrations_service/app/router.py
@@ -5,11 +5,8 @@ from core.usecases.tiktok_integration import TikTokIntegrationUseCase
 
 api_router = APIRouter(prefix="/integrations")
 
-@api_router.post("/tiktok/callback",
-                 status_code=status.HTTP_200_OK,
-                 tags=["TikTok"]
-                 )
-async def tiktok_integration_callback(
+@api_router.post("/tiktok/start", status_code=status.HTTP_201_CREATED, tags=["TikTok"])
+async def tiktok_integration_start(
 	tiktok_integration_usecase: TikTokIntegrationUseCase = Depends(get_tiktok_integration_usecase),
 	code: str = Query(alias="code", description="TikTok authorization code")):
 
