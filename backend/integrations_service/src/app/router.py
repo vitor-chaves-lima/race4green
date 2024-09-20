a63b09c4785f30a2c7c375746c8a3fe90f2d2708
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-20 00:32:13 -0300

    Adicionando modelo de requests

--- backend/integrations_service/src/app/router.py
+++ backend/integrations_service/src/app/router.py
@@ -1,13 +1,16 @@
-from fastapi import APIRouter, Depends, Query, status
+from urllib.parse import unquote
+from fastapi import APIRouter, Depends, status
 
 from app.dependencies import get_tiktok_integration_usecase
+from app.requests import TokenRequest
 from core.usecases.tiktok_integration import TikTokIntegrationUseCase
 
 api_router = APIRouter(prefix="/integrations")
 
 @api_router.post("/tiktok/start", status_code=status.HTTP_201_CREATED, tags=["TikTok"])
 async def tiktok_integration_start(
-	tiktok_integration_usecase: TikTokIntegrationUseCase = Depends(get_tiktok_integration_usecase),
-	code: str = Query(alias="code", description="TikTok authorization code")):
+	token_request: TokenRequest,
+	tiktok_integration_usecase: TikTokIntegrationUseCase = Depends(get_tiktok_integration_usecase)):
 
-	tiktok_integration_usecase.get_token(user_id="test", code=code)
+	code = token_request.code
+	tiktok_integration_usecase.get_token(user_id="test", code=unquote(code))
