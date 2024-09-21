fa1ed6b02c1ab4392fa3d7d42d028ff85c0d8029
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-21 06:09:59 -0300

    Adicionando endpoint /tiktok/init

--- backend/integrations_service/app/router.py
+++ backend/integrations_service/app/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status
+from fastapi.responses import RedirectResponse
 
 from app.dependencies import get_tiktok_integration_usecase
 from app.requests import TokenRequest
@@ -7,8 +8,23 @@ from core.usecases.tiktok_integration import TikTokIntegrationUseCase
 
 api_router = APIRouter(prefix="/integrations")
 
-@api_router.post("/tiktok/start", status_code=status.HTTP_201_CREATED, tags=["TikTok"])
-async def tiktok_integration_start(
+@api_router.get("/tiktok/init",
+				responses={
+					302: {"description": "Redireciona para a página de autenticação do TikTok"},
+					400: {"description": "Erro ao gerar a URL de autorização do TikTok"},
+				})
+async def tiktok_integration_init(
+	tiktok_integration_usecase: TikTokIntegrationUseCase = Depends(get_tiktok_integration_usecase)):
+
+	user = User(user_id="test")
+
+	authorize_url = tiktok_integration_usecase.get_authorize_url(user)
+
+	return RedirectResponse(authorize_url.authorize_url, status.HTTP_302_FOUND)
+
+
+@api_router.post("/tiktok/callback", status_code=status.HTTP_201_CREATED, tags=["TikTok"])
+async def tiktok_integration_callback(
 	token_request: TokenRequest,
 	tiktok_integration_usecase: TikTokIntegrationUseCase = Depends(get_tiktok_integration_usecase)):
 
--- backend/integrations_service/core/usecases/tiktok_integration.py
+++ backend/integrations_service/core/usecases/tiktok_integration.py
@@ -1,17 +1,52 @@
+import secrets
+
+from core.db.repositories.csrf_state import CSRFStateRepository
 from core.db.repositories.tiktok_token import TikTokTokenRepository
-from core.models.tiktok_integration import TikTokUser, TikTokToken, TikTokTokenType
+from core.models.tiktok_integration import TikTokUser, TikTokToken, TikTokTokenType, AuthorizeUrl
 from core.models.user import User
 from core.services.tiktok_integration import TikTokIntegrationService
+from core.settings import TikTokIntegrationSettings
 
 
 class TikTokIntegrationUseCase:
+	_settings: TikTokIntegrationSettings
+	_csrf_state_repository: CSRFStateRepository
 	_token_repository: TikTokTokenRepository
 	_tiktok_integration_service: TikTokIntegrationService
 
-
-	def __init__(self, token_repository: TikTokTokenRepository, tiktok_integration_service: TikTokIntegrationService):
+	def __init__(self,
+				 settings: TikTokIntegrationSettings,
+				 csrf_state_repository: CSRFStateRepository,
+				 token_repository: TikTokTokenRepository,
+				 tiktok_integration_service: TikTokIntegrationService):
+		self._settings = settings
+		self._csrf_state_repository = csrf_state_repository
 		self._token_repository = token_repository
 		self._tiktok_integration_service = tiktok_integration_service
+
+
+	_tiktok_integration_service: TikTokIntegrationService
+
+	@staticmethod
+	def _generate_csrf_state(length: int) -> str:
+		return secrets.token_urlsafe(length)
+
+
+	def get_authorize_url(self, user: User) -> AuthorizeUrl:
+		csrf_state = self._generate_csrf_state(32)
+
+		self._csrf_state_repository.store_state(user, csrf_state)
+
+		authorize_url = (
+			f"https://www.tiktok.com/v2/auth/authorize/"
+			f"?client_key={self._settings.client_key}"
+			f"&scope=user.info.basic"
+			f"&response_type=code"
+			f"&redirect_uri={self._settings.redirect_uri}"
+			f"&state={csrf_state}"
+		)
+
+		return AuthorizeUrl(authorize_url=authorize_url, csrf_state=csrf_state)
 
 
 	def get_token(self, user: User, code: str):
