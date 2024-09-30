dbbc6c3d5fd27bafde2e6f8071a45ae857de0a9f
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-30 14:00:31 -0300

    Alterando estrutura de pastas

---
+++ frontend/web_app/src/app/utils/consts.ts
@@ -0,0 +1,7 @@
+/*---------------- CONST'S ------------------*/
+
+const API_GATEWAY_URL = new URL(`http://${import.meta.env.VITE_API_GATEWAY_URL}`)
+
+/*----------------- EXPORTS -----------------*/
+
+export { API_GATEWAY_URL }
