b6f64b27c816c910b81f462a1bacf5477578cb1b
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-30 15:53:00 -0300

    Hotfix web app docker (#29)

--- frontend/web_app/src/app/utils/consts.ts
+++ frontend/web_app/src/app/utils/consts.ts
@@ -1,7 +1,16 @@
+function ensureHttps(url: string) {
+	if (!/^https?:\/\//i.test(url)) {
+		return `https://${url}`;
+	}
+	return url;
+}
+
 /*---------------- CONST'S ------------------*/
 
-const API_GATEWAY_URL = new URL(`http://${import.meta.env.VITE_API_GATEWAY_URL}`)
+const API_GATEWAY_URL = new URL(
+	ensureHttps(import.meta.env.VITE_API_GATEWAY_URL),
+);
 
 /*----------------- EXPORTS -----------------*/
 
-export { API_GATEWAY_URL }
+export { API_GATEWAY_URL };
