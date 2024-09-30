82f5b7ff67be1733eafcc5311169346141482a57
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-09-30 15:49:51 -0300

    Corrigindo build da web app

--- frontend/web_app/src/app/utils/consts.ts
+++ frontend/web_app/src/app/utils/consts.ts
@@ -1,7 +1,7 @@
 /*---------------- CONST'S ------------------*/
 
-const API_GATEWAY_URL = new URL(`http://${import.meta.env.VITE_API_GATEWAY_URL}`)
+const API_GATEWAY_URL = new URL(`${import.meta.env.VITE_API_GATEWAY_URL}`);
 
 /*----------------- EXPORTS -----------------*/
 
-export { API_GATEWAY_URL }
+export { API_GATEWAY_URL };
